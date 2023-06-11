c3da47f1e8f647583b18313fd7909b522a892794
adamakerman <95adam.akeraman@gmail.com>
2023-06-11 17:46:35 +0200

    removed unused imports

--- src/main/java/com/example/springboot/ChurchController.java
+++ src/main/java/com/example/springboot/ChurchController.java
@@ -4,11 +4,9 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.UnsupportedEncodingException;
 
 @RestController
 public class ChurchController {
